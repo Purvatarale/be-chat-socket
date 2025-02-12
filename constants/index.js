192ce70cb27b61cd05c3d1f21a1b6a07021ddfa5
Purvatarale <purvatarale23@gmail.com>
2025-02-12 23:41:47 +0530

    category-ids updated

--- constants/index.js
+++ constants/index.js
@@ -1,42 +1,42 @@
 const CHAT_CATEGORIES = [
   {
     title: "Academics",
-    id: "c4PNYJUdqwfLL6y9wRLgK3Zd",
+    id: "42CgevrWHwLZhpoMbnq2rCc1",
     icon: "https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Student-3-1024.png",
     description:
       "Discuss courses, exams, and academic resources with peers and mentors.",
   },
   {
     title: "BASIS",
-    id: "jqdviogqeBMzB69Kvotmq6y8",
+    id: "q9SvnVX3oUWhmdcfqNQKP5xg",
     icon: "https://cdn-icons-png.freepik.com/512/11946/11946169.png?fd=1&filename=progress_11946169.png",
     description:
       "Manages ERP accounts, roles, and licenses for seamless system access.",
   },
   {
     title: "Fees",
-    id: "ofyFouis1rXW5RGd2mDYyLx9",
+    id: "kwkchQtznUWxzBLyMtTfTiS2",
     icon: "https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Money-Increase-1024.png",
     description:
       "Get information on tuition, fee deadlines, scholarships, and payments.",
   },
   {
     title: "HCM",
-    id: "EcpRsRKotEbfTCscbLThKwev",
+    id: "eTx36po5q7KCozcxVnmUErdF",
     icon: "https://cdn-icons-png.freepik.com/512/10269/10269894.png?fd=1&filename=headhunting_10269894.png",
     description:
       "Handles leaves, claims, and travel arrangements for smooth organizational operations.",
   },
   {
     title: "Hospital",
-    id: "6dvQ2grnhsQQxWTUGVvTCvGn",
+    id: "f91GenWMA3KvdyXBgjkZrD6G",
     icon: "https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Surgeon-1024.png",
     description:
       "Access resources for healthcare, hospital services, and medical assistance.",
   },
   {
     title: "Hostel",
-    id: "GiyDWc8BAVDqfJ3MuQodx2c1",
+    id: "eNfYujo9gByY7zymQUKxso7a",
     icon: "https://cdn3.iconfinder.com/data/icons/luchesa-vol-9/128/Home-1024.png",
     description:
       "Ask questions about hostel life, room allocations, and facilities.",
